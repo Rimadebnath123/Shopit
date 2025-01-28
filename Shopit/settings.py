00f9b73972f80f698c8052f4a269c8dbf3d41b07
Rimadebnath123 <rimacse5@gmail.com>
2025-01-28 10:04:35 +0530

    add product in admin

--- Shopit/settings.py
+++ Shopit/settings.py
@@ -39,17 +39,27 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'shop_app',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:8080",
+"http://localhost:5173",
+"http://localhost:5174",
 ]
 
 ROOT_URLCONF = 'Shopit.urls'
